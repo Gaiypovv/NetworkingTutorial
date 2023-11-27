caa38c1b4d686ca0325e45c6d0f7f69e43425a9f
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-27 15:00:36 +0600

    Listed the coins in one raw wich was fethed from the api coin gecko

---
+++ NetworkingTutorial/Core/AllCoins/Model/Coin.swift
@@ -0,0 +1,8 @@
+//
+//  Coin.swift
+//  NetworkingTutorial
+//
+//  Created by Eldar Gaiypov on 27/11/23.
+//
+
+import Foundation
---
+++ NetworkingTutorial/Core/AllCoins/Service/CoinDataService.swift
@@ -0,0 +1,8 @@
+//
+//  CoinDataService.swift
+//  NetworkingTutorial
+//
+//  Created by Eldar Gaiypov on 27/11/23.
+//
+
+import Foundation
