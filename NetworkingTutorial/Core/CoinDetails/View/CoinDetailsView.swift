28c56858def1895246ffb01a5c3966e3f6d533dd
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-30 11:57:34 +0600

    ADDED the navigation link to the project in order to open new page to see datails info about every coin

---
+++ NetworkingTutorial/Core/CoinDetails/View/CoinDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinDetailsView.swift
+//  NetworkingTutorial
+//
+//  Created by Eldar Gaiypov on 30/11/23.
+//
+
+import SwiftUI
+
+struct CoinDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinDetailsView()
+}
