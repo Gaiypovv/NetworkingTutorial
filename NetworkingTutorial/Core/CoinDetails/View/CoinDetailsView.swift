e0027eb03bef4463e8e7902cff9a23a2d961d1f8
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-30 12:00:34 +0600

    Added the the dependecy injection to the project

--- NetworkingTutorial/Core/CoinDetails/View/CoinDetailsView.swift
+++ NetworkingTutorial/Core/CoinDetails/View/CoinDetailsView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct CoinDetailsView: View {
+    let coin: Coin
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(coin.name)
     }
 }
 
-#Preview {
-    CoinDetailsView()
-}
+//#Preview {
+//    CoinDetailsView()
+//}
