b15a27c5fa08c35745d8c3dbcd360c0343929cc7
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-30 13:06:27 +0600

    Added the function to see the detail info about every coin

--- NetworkingTutorial/Core/AllCoins/Service/CoinDataService.swift
+++ NetworkingTutorial/Core/AllCoins/Service/CoinDataService.swift
@@ -11,7 +11,6 @@ class CoinDataService {
     
     private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h&locale=en"
     
-    private let detailsUrlString = "https://api.coingecko.com/api/v3/coins/bitcoin?tickers=false"
     
     func fetchCoins() async throws -> [Coin] {
         guard let url = URL(string: urlString) else { return [] }
@@ -36,6 +35,8 @@ class CoinDataService {
     }
     
     func fetchCoinDetails(id: String) async throws -> CoinDetails? {
+        let detailsUrlString = "https://api.coingecko.com/api/v3/coins/\(id)?tickers=false"
+        
         guard let url = URL(string: detailsUrlString) else { return nil  }
         
         let (data, response) = try await URLSession.shared.data(from: url)
