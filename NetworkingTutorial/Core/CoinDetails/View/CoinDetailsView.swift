7c6e40c49866f96a978190c8ba1f7d14f8734c37
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-30 13:29:22 +0600

    Added the task modifieres to do the simple stop request when for example user tap the back button immedeiately

--- NetworkingTutorial/Core/CoinDetails/View/CoinDetailsView.swift
+++ NetworkingTutorial/Core/CoinDetails/View/CoinDetailsView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct CoinDetailsView: View {
     let coin: Coin
     @ObservedObject var viewModel: CoinDetailsViewModel
-    @State private var task: Task<(), Never>?
     
     init(coin: Coin) {
         self.coin = coin
@@ -32,12 +31,15 @@ struct CoinDetailsView: View {
                     .padding(.vertical)
             }
         }
-        .onAppear {
-            self.task = Task { await viewModel.fetchCoinDetails() }
+        .task {
+            await viewModel.fetchCoinDetails()
         }
-        .onDisappear() {
-            task?.cancel()
-        }
+//        .onAppear {
+//            self.task = Task { await viewModel.fetchCoinDetails() }
+//        }
+//        .onDisappear() {
+//            task?.cancel()
+//        }
         .padding()
     }
 }
